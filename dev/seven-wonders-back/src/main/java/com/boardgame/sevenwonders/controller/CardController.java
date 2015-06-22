4d40c956dd187dcc5f4fc223b4af563e00c9fc10
raptorsun <raptorsun@gmail.com>
2015-06-23 00:12:51 +0200

    Card controller uses card service
    Just get a list of cards

--- dev/seven-wonders-back/src/main/java/com/boardgame/sevenwonders/controller/CardController.java
+++ dev/seven-wonders-back/src/main/java/com/boardgame/sevenwonders/controller/CardController.java
@@ -3,6 +3,10 @@ package com.boardgame.sevenwonders.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,19 +14,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.boardgame.sevenwonders.model.Card;
+import com.boardgame.sevenwonders.service.CardService;
+import com.boardgame.sevenwonders.service.CardServiceImpl;
+
 
 @RestController
 @RequestMapping(value = "/api/rest/card")
 public class CardController {
 	
+	@Resource
+	CardService cardService;
+	
 	@RequestMapping(value = "/all", method = RequestMethod.GET)
 	public ResponseEntity<List<Card>> getAll() {
 		try {
 			// should be in a service, to do later
-			List<Card> cardList = new ArrayList<>();
-			cardList.add(new Card(0, "Lumber Yard", 1));
-			cardList.add(new Card(1, "Glassworks", 2));
-			cardList.add(new Card(2, "Arsenal", 3));
+			List<Card> cardList = cardService.getAllCards() ;
 			
 			return new ResponseEntity<List<Card>>(cardList, HttpStatus.OK);
 		} catch (RuntimeException e) {
